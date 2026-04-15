46befcb723d85db658c162958d1c1705c9a728f6
Tofutush <siroccoxxq@gmail.com>
2026-04-14 20:19:39 -0400

    pathprefix

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 import { VentoPlugin } from "eleventy-plugin-vento";
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
 	eleventyConfig.setQuietMode(true);
 	eleventyConfig.addPlugin(VentoPlugin);
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 	eleventyConfig.addPassthroughCopy("style.css");
 	return {
 		markdownTemplateEngine: 'vto',
